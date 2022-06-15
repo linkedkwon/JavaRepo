c69a68ddb6b35e849f81f0bc509d0962a0665e2c
linkedkwon <linkedkwon@gmail.com>
2022-06-16 05:25:32 +0900

    Class

---
+++ src/oop/ClassCase.java
@@ -0,0 +1,14 @@
+package oop;
+
+class Product{
+    //...
+}
+
+public class ClassCase {
+
+    //case 1 : Instantiate
+    public static void main(String[] args){
+        Product obj = new Product();
+    }
+
+}
